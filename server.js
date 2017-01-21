3e077a5b31f7fa5a626f8301500ed84fae74838e
Rohan Thorat <rthorat1@binghamton.edu>
2017-01-21 14:55:51 -0500

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require("underscore");
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -48,17 +49,24 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
 
 		var requestedBody = _.pick(req.body,'description', 'completed');
-		var body = requestedBody;
 
-		if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-			return res.status(404).send();
-		}
 
-		body.description = body.description.trim();
-		body.id = todoNextId;
-		todoNextId++;
-		todos.push(body);
-		res.json(todos);
+		db.todo.create(requestedBody).then(function (todo) {
+			res.json(todo.toJSON());
+		}, function (e) {
+			res.status(400).json(e);
+		});
+		// var body = requestedBody;
+
+		// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		// 	return res.status(404).send();
+		// }
+
+		// body.description = body.description.trim();
+		// body.id = todoNextId;
+		// todoNextId++;
+		// todos.push(body);
+		// res.json(todos);
 });
 
 app.delete('/todos/:id', function (req, res) {
@@ -101,8 +109,11 @@ app.put('/todos/:id', function (req, res) {
 });
 
 
-app.listen(PORT, function () {
-
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function () {
+		app.listen(PORT, function () {
+			console.log('Express listening on port ' + PORT + '!');
+	});	
 });
+
+
 
