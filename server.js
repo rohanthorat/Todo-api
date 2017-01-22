c70c164b88160faa0f3a1627e377a4e6c419b76f
Rohan Thorat <rthorat1@binghamton.edu>
2017-01-21 19:09:55 -0500

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -56,7 +56,6 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	
-
 	db.todo.findById(todoId).then(function (todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
@@ -84,14 +83,32 @@ app.post('/todos', function (req, res) {
 
 app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if (!matchedTodo) {
-		res.status(404).json({"error": "no todo found with that id"});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function () {
+		res.status(500).send();
+	});
+
+
+	// var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	// if (!matchedTodo) {
+	// 	res.status(404).json({"error": "no todo found with that id"});
+	// } else {
+	// 	todos = _.without(todos, matchedTodo);
+	// 	res.json(matchedTodo);
+	// }
 });
 
 
